558f9b4625d5f580182623eded395f50f20b3c3c
Hung-Jin Huang <hungjinh@users.noreply.github.com>
2022-10-04 13:50:51 -0400

    dcgan developments (#1)
    * basic build-ups
    
    * training command line script
    
    * add lr_scheduler
    
    * adjust parameter names used in config file and in codes (_g/_d -> _G/_D)
    
    * [minor] modify print out text
    
    * [minor] add suggested training parameter settings for actual runs in #(...)
    
    * [minor] add dcgan reference
    
    * DCGAN_Generator.gen_galaxy method
    
    * [minor] run0 scripts
    
    * run1 configs (constant lr)
    
    * run 1.1 config
    
    * run2 config
    
    * run3 config
    
    * update demo notebooks of dcgan training
    
    * display the best trained model of dcgan

---
+++ bin/train_dcgan.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path+'/..')
+
+from galaxy_generator.dcgan_generator import DCGAN_Generator
+from galaxy_generator.utils import get_config_from_yaml
+
+
+def main(args=None):
+    '''
+        Usage:
+            run a test (on genie)
+            >> cd /home/hhg/Research/galaxyClassify/repo/GalaxyZooGenerator/
+            >> mkdir experiments
+            >> python3 bin/train_dcgan.py --config configs/dcgan_test.yaml > experiments/test.log
+            >> python3 bin/train_dcgan.py --config configs/dcgan_run0.yaml > experiments/run0.log
+            >> python3 bin/train_dcgan.py --config configs/dcgan_run3.yaml > experiments/run3.log
+        
+        p.s. To delete the test file
+            >> rm -rf ./experiments/TestDCGAN/
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True,
+                        help='config file for training settings')
+
+    opt = parser.parse_args()
+    print(opt)
+
+    config = get_config_from_yaml(opt.config)
+    generator = DCGAN_Generator(config=config)
+    generator.train()
+
+
+if __name__ == '__main__':
+    main()
